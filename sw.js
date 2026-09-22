9c9dd10489da81e5e7fa8d216a0a28fb51045edf
kklmwd <koko.liem@gmail.com>
2026-09-22 09:27:48 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "ppl-log-v1";
+const CACHE = "ppl-log-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
